88caba7ccf163e63ea2aa322e84b5873c4d9ab1c
engRana404 <RanaGamalDaif@gmail.com>
2025-08-30 02:46:41 +0300

    add HomePage class for search functionality using Selenium

---
+++ src/main/java/com/vois/pages/HomePage.java
@@ -0,0 +1,29 @@
+package com.vois.pages;
+
+import com.vois.utils.LocatorReaderUtils;
+import com.vois.utils.actions.BrowserActions;
+import com.vois.utils.actions.ElementActions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class HomePage {
+
+    private WebDriver driver;
+
+    // Locators
+    private static final By searchBox = LocatorReaderUtils.getLocator("home", "searchBox");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Convenience method: perform a search in one step
+    public void search(String keyword) {
+        WebElement box = driver.findElement(searchBox);
+        box.sendKeys(keyword);
+        box.submit(); // This triggers the form submission
+    }
+}
