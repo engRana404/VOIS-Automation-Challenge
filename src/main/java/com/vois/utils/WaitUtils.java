1bf6d0427c1d94d926d00e0163118ca8e0f641b3
engRana404 <RanaGamalDaif@gmail.com>
2025-08-29 22:25:08 +0300

    add WaitUtils class for Selenium element wait utilities

---
+++ src/main/java/com/vois/utils/WaitUtils.java
@@ -0,0 +1,31 @@
+package com.vois.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+    private WebDriver driver;
+    private static final int DEFAULT_WAIT_TIME_SECONDS = 10;
+
+    private WaitUtils() {
+        // Prevent instantiation
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    // Method to wait for an element to be visible
+    public static WebElement waitForElementToBeVisible(WebDriver driver, By locator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_WAIT_TIME_SECONDS))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    // Method to wait for an element to be clickable
+    public static WebElement waitForElementToBeClickable(WebDriver driver, By locator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_WAIT_TIME_SECONDS))
+                .until(ExpectedConditions.elementToBeClickable(locator));
+    }
+}
