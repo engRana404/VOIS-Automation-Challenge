2d842ab89f1fcf9bac154f91ca6aff2409103f09
engRana404 <RanaGamalDaif@gmail.com>
2025-08-30 18:59:05 +0300

    update VodafoneSearchTest to use PropertiesUtils for base URL retrieval and remove baseUrl from searchData.json

--- src/test/java/com/vois/tests/VodafoneSearchTest.java
+++ src/test/java/com/vois/tests/VodafoneSearchTest.java
@@ -3,6 +3,7 @@ package com.vois.tests;
 import com.vois.pages.HomePage;
 import com.vois.pages.ResultsPage;
 import com.vois.utils.JsonUtils;
+import com.vois.utils.PropertiesUtils;
 import com.vois.utils.ValidationUtils;
 import com.vois.utils.actions.BrowserActions;
 import org.openqa.selenium.WebDriver;
@@ -18,7 +19,7 @@ public class VodafoneSearchTest {
     private HomePage homePage;
     private ResultsPage resultsPage;
 
-    private static final String BASE_URL = JsonUtils.getTestData("baseUrl");
+    private static final String BASE_URL = PropertiesUtils.getPropertyValue("baseUrl");
     private static final String SEARCH_KEYWORD = JsonUtils.getTestData("searchKeyword");
     private static final String EXPECTED_RELATED_TEXT = JsonUtils.getTestData("expectedRelatedText");
     private static final int EXPECTED_RELATED_SECTIONS_COUNT = Integer.parseInt(JsonUtils.getTestData("expectedRelatedCount"));
