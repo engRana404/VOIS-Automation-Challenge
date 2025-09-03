d39f6ee08c49d523d1e78bce69dfbe09e3b42cc6
engRana404 <RanaGamalDaif@gmail.com>
2025-09-03 23:13:43 +0300

    Refactor RelatedSearchTest to streamline page object usage and improve readability

--- src/test/java/com/vois/tests/RelatedSearchTest.java
+++ src/test/java/com/vois/tests/RelatedSearchTest.java
@@ -4,15 +4,11 @@ import com.vois.drivers.DriverManager;
 import com.vois.pages.HomePage;
 import com.vois.pages.ResultsPage;
 import com.vois.utils.*;
-import com.vois.utils.actions.BrowserActions;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class RelatedSearchTest {
     private WebDriver driver;
-    private HomePage homePage;
-    private ResultsPage resultsPage;
 
     private static final String SEARCH_KEYWORD = JsonUtils.getTestData("searchKeyword");
     private static final String EXPECTED_RELATED_TEXT = JsonUtils.getTestData("expectedRelatedText");
@@ -21,19 +17,19 @@ public class RelatedSearchTest {
     @BeforeMethod(alwaysRun = true)
     public void setup() {
         driver = DriverManager.createInstance();
-        homePage = new HomePage(driver);
-        resultsPage = new ResultsPage(driver);
 
-        homePage.search(SEARCH_KEYWORD);
+        new HomePage(driver)
+                .navigateToHomePage()
+                .search(SEARCH_KEYWORD);
     }
 
     @Test
     public void validateRelatedSearchSections() {
-        boolean relatedSectionsValid = resultsPage.validateRelatedSearchSections(
-                EXPECTED_RELATED_TEXT,
-                EXPECTED_RELATED_SECTIONS_COUNT
-        );
-        Assert.assertTrue(relatedSectionsValid, "Related search sections validation failed!");
+        new ResultsPage(driver)
+                .validateRelatedSearchSections(
+                    EXPECTED_RELATED_TEXT,
+                    EXPECTED_RELATED_SECTIONS_COUNT
+                );
     }
 
     @AfterMethod(alwaysRun = true)
